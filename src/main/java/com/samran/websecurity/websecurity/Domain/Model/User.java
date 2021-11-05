fa316c9c6c88af20ad86912c1f6e9f0babb4eb36
samuran <samurana@fsi.co.jp>
2021-11-05 17:15:39 +0900

    added user class

---
+++ src/main/java/com/samran/websecurity/websecurity/Domain/Model/User.java
@@ -0,0 +1,33 @@
+package com.samran.websecurity.websecurity.Domain.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="user")
+
+public class User {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(max=40)
+	private String email;
+	
+	@NotBlank
+	@Size(min = 8, max = 16)
+	private String password;
+}
---
+++ src/test/java/com/samran/websecurity/websecurity/WebsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samran.websecurity.websecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
